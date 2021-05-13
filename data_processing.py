fe35cd0234aa657b8c52921d2e5b11c08e43c7a3
Terence <68271470+tsamba120@users.noreply.github.com>
2021-05-12 20:35:48 -0700

    Actions During Stop Processed

--- data_processing.py
+++ data_processing.py
@@ -42,7 +42,7 @@ def clean_table_0():
     Cleans mainStopDateTime
     '''
     df_main = load_process_tables('0. mainStopDateTime.csv')
-    print('Table 0 loaded\n')
+    print('Table 0 loaded...')
     df_main['stopDate'] = pd.to_datetime(df_main['stopDate']).dt.date
     df_main['stopTime'] = pd.to_datetime(df_main['stopTime']).dt.time
     cols_to_drop = ['highwayExit','isSchool', 'schoolName', 'isStudent']
@@ -57,7 +57,7 @@ def clean_table_1():
     Cleans perceivedRace table
     '''
     df_race = load_process_tables('1. perceivedRace.csv')
-    print('Table 1 loaded\n')
+    print('Table 1 loaded...')
     df_race = pd.get_dummies(df_race).groupby(['StopID', 'PID']).sum()
     df_race.rename(columns= lambda x: re.sub('perceivedRace_', 'perceived_', x))
     df_race.to_csv(out_file_path + 'PROCESSED_1_race.csv')
@@ -69,7 +69,7 @@ def clean_table_2():
     Cleans perceived disability table
     '''
     disability_df = load_process_tables('2. perceivedKnownDisability.csv')
-    print('Table 2 loaded\n')
+    print('Table 2 loaded...')
     disability_df.rename(columns= lambda x: re.sub('perceivedOrKnown', '', x), inplace=True)
     disability_df = pd.get_dummies(disability_df)
     disability_df.drop('Disability_None', axis=1, inplace=True)
@@ -83,7 +83,7 @@ def clean_table_3():
     Cleans reason for stop data
     '''
     rfs_df = load_process_tables('3. reasonForStop.csv')
-    print('Table 3 loaded\n')
+    print('Table 3 loaded...')
     rfs_df = pd.get_dummies(rfs_df.drop(['ObjectId','reasonForStopcode', 'reasonForStopCodeText'], axis=1)).groupby(['StopID','PID']).sum()
     rfs_df.rename(columns=lambda x: re.sub('reasonForStop_', 'RFS_', x), inplace=True)
     rfs_df.rename(columns=lambda x: re.sub('reasonForStopDetail_', 'RFS_Detail_', x), inplace=True)
@@ -95,7 +95,27 @@ def clean_table_4():
     '''
     Cleans actions during stop
     '''
+    ads_df = load_process_tables('4. actionsDuringStop.csv')
+    print('Table 4 loaded...')
     
+    # First half
+    ads_df1 = pd.get_dummies(ads_df[['StopID', 'PID', 'actionTakenDuringStop']]).groupby(
+        ['StopID', 'PID']
+    ).sum().reset_index()
+    ads_df1.rename(columns= lambda x: re.sub('actionTakenDuringStop_', 'Action_', x), inplace=True)
+    ads_df1.drop('Action_None', axis=1, inplace=True)
+
+    # Non-dummy columns to keep
+    ads_df2 = ads_df.drop(['ObjectId', 'actionTakenDuringStop'], axis=1).drop_duplicates()
+    ads_df2['personSearchConsentGiven'] = ads_df2['personSearchConsentGiven'].apply(float)
+    ads_df2['propertySearchConsentGiven'] = ads_df2['propertySearchConsentGiven'].apply(float)
+    ads_df2 = ads_df2.goupby(['StopID', 'PID']).sum().reset_index()
+    ads_df2['personSearchConsentGiven'] = ads_df2['personSearchConsentGiven'].apply(int)
+    ads_df2['propertySearchConsentGiven'] = ads_df2['propertySearchConsentGiven'].apply(int)
+
+    final_ads_df = ads_df1.merge(ads_df2, on=['StopID', 'PID'])
+    final_ads_df.to_csv(out_file_path + 'PROCESSED_4_actionDurintStop.csv')
+    print('Table 4 processed\n')
 
 
 if __name__ == '__main__':
