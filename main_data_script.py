c29e549e42db5f55349e9f98b7ef0c301879a7e2
Terence <68271470+tsamba120@users.noreply.github.com>
2021-05-14 00:08:32 -0700

    5/13/21 - Data Processing Commit

--- main_data_script.py
+++ main_data_script.py
@@ -6,5 +6,5 @@ from api_connect import url_dict, callAPI
 import data_processing as dp
 
 if __name__ == "__main__":
-    callAPI(url_dict)
-    dp.main()
+    callAPI(url_dict) # connects to API
+    dp.main() # processes raw tables
